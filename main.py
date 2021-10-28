fe04beb669920d4ad869a073b0d5bcbb80b26c6b
tadashi.kurino <t.kurino@gmail.com>
2021-10-28 23:42:40 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# credential GCP spred drive API
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credential = ServiceAccountCredentials.from_json_keyfile_name(
+    'auth/active-chimera-330406-5d2c982b26f0.json', scope)
+# googleSpredShhet
+gspreadInstance = gspread.authorize(credential)
